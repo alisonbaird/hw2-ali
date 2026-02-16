a42a9cd6efcbd409acb2433f2e735bb0e3559ed4
alisonbaird <ali.baird@kellogg.northwestern.edu>
2026-02-16 04:45:47 +0000

    did as much as i could

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+movies = Movie.create({ name: "Batman Begins" }, {name: "The Dark Knight"}, {name: "The Dark Knight Rises"})
